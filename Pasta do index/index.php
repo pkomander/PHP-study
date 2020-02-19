ff23a59756b4a25cc079da8b24b8385ad6b24ca2
Paulo Ricardo Silva de Araújo <54222357+pkomander@users.noreply.github.com>
2020-02-19 02:08:27 -0300

    pasta index
    pasta index

---
+++ Pasta do index/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+
+	</head>
+
+	<body>
+
+	<?php 
+		echo 'utilizando a tag padrão';
+	?>
+
+	<br>
+
+	<?= 'utilizando a tag impressão' ?>
+
+	<br>
+
+	<? echo 'utilizando a tag curta' ?>
+
+	</body>
+</html>
